053685c4b82b298e2894faba1b4915d161d48ecf
Kenneth Berland <ken@grandrounds.com>
2016-03-06 12:28:58 -0800

    add tags

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160222034310) do
+ActiveRecord::Schema.define(version: 20160306202419) do
+
+  create_table "tags", force: :cascade do |t|
+    t.string   "tag"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "tags", ["tag"], name: "index_tags_on_tag", unique: true
 
   create_table "users", force: :cascade do |t|
     t.string   "uid"
