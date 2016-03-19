021f37d59dfeafa4c0de54b62aaa9556061868d1
Kenneth Berland <ken@grandrounds.com>
2016-03-18 18:22:48 -0700

    move the new tag form to the tag library carousel.

--- app/controllers/tag_library_controller.rb
+++ app/controllers/tag_library_controller.rb
@@ -2,6 +2,20 @@ class TagLibraryController < ApplicationController
   protect_from_forgery with: :exception
 
   def show
+    if params[:category] == 'new-tag-form'
+      render_new_tag_form
+    else
+      render_tag_category
+    end
+  end
+
+  private
+
+  def render_new_tag_form
+    render partial: 'new_tag_form'
+  end
+
+  def render_tag_category
     @tags = Tag.joins(:tag_category).where(tag_categories: { category: params[:category] })
     @tag_category = TagCategory.find_by_category(params[:category]).category
     render layout: false
