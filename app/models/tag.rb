bdd2c793e8df81a8ba75eea62dcba272b484fbc6
Kenneth Berland <ken@grandrounds.com>
2016-03-19 18:31:30 -0700

    wip

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,7 +1,6 @@
 class Tag < ActiveRecord::Base
   belongs_to :tag_category, class_name: 'TagCategory'
 
-
   def count(user_id)
     UserTag.where(tag_id: id).where(to_user_uid: user_id).group([:to_user_uid, :tag_id]).count(:tag_id).values.first
   end
