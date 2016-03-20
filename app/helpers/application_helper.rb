fac7574d9ee6381f4e845f2b9c326734261ba876
Justin Ahn <justin@soonahn.ca>
2016-03-20 16:00:30 -0700

    able to drag in more places than just the small bar, added colours, added fa tags

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,23 @@ module ApplicationHelper
   def current_user
     self.controller.current_user
   end
+
+  def font_awesomeify(category)
+    case category
+    when 'finance'
+      'fa-money'
+    when 'work'
+      'fa-briefcase'
+    when 'relationships'
+      'fa-heart'
+    when 'school'
+      'fa-book'
+    when 'sports'
+      'fa-futbol-o'
+    when 'roommates'
+      'fa-users'
+    else
+      'fa-question-circle'
+    end
+  end
 end
