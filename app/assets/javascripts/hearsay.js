9ff5137b621b6f0807d131f22b1d4c6c0bf651d6
Justin Ahn <justin@soonahn.ca>
2016-03-21 21:52:32 -0700

    adding the active_user as a data callback

--- app/assets/javascripts/hearsay.js
+++ app/assets/javascripts/hearsay.js
@@ -3,21 +3,36 @@ $(function() {
     on('ajax:success',function(evt, data, status, xhr){
       $("#"+evt.target.dataset.destroy).remove();
     });
+
   $(".user-profile").each(function(i) {
-    $(this).load("/connections/" + i)
-  })
+    $(this).load("/connections/" + i);
+  });
+
   $(".tag_library").each(function(i) {
-    $(this).load("/tag_library/" + this.dataset.category )
-  })
+    $(this).load("/tag_library/" + this.dataset.category );
+  });
+
   $(".owl-carousel").owlCarousel({
     items: 1,
-    dots: false
+    dots: false,
+    onDragged: owlCallback
   });
-  $('body').bind('touchmove', function(e){e.preventDefault()})
+
+  $('body').bind('touchmove', function(e){e.preventDefault()});
 });
 
+function owlCallback(event){
+  var eventName = event.type;
+
+  if(eventName == 'dragged') {
+    active_id = $('.owl-item.active > .user-profile > .row').attr('id');
+    $('.active-user').data('active_user', active_id);
+
+  }
+};
+
 function addNewTagToCurrentConnection(tagId) {
-  var $connection = $('.active-user').data().active_user
+  var $connection = $('#active-user').data().active_user;
   $.ajax({
     method: "POST",
     url: "/users/" + $connection + "/tags",
