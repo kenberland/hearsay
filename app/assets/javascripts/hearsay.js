56f6d3966dfc7df9ca9ec8fe7c485900f0dabf00
Justin Ahn <justin@soonahn.ca>
2016-03-21 22:18:13 -0700

    fixed my broken behaviour

--- app/assets/javascripts/hearsay.js
+++ app/assets/javascripts/hearsay.js
@@ -5,11 +5,16 @@ $(function() {
     });
 
   $(".user-profile").each(function(i) {
-    $(this).load("/connections/" + i);
+    $(this).load("/connections/" + i, function(response, status, xhr){
+      if ( !$('#active-user').data('active_user') ) {
+        active_id = $(response).attr('id');
+        $('#active-user').data('active_user', active_id);
+      }
+    });
   });
 
   $(".tag_library").each(function(i) {
-    $(this).load("/tag_library/" + this.dataset.category );
+    $(this).load("/tag_library/" + this.dataset.category);
   });
 
   $(".owl-carousel").owlCarousel({
@@ -18,21 +23,20 @@ $(function() {
     onDragged: owlCallback
   });
 
-  $('body').bind('touchmove', function(e){e.preventDefault()});
+  $('body').bind('touchmove', function(e){e.preventDefault();});
 });
 
 function owlCallback(event){
   var eventName = event.type;
 
-  if(eventName == 'dragged') {
+  if ( eventName == 'dragged' ) {
     active_id = $('.owl-item.active > .user-profile > .row').attr('id');
-    $('.active-user').data('active_user', active_id);
-
+    $('#active-user').data('active_user', active_id);
   }
 };
 
 function addNewTagToCurrentConnection(tagId) {
-  var $connection = $('#active-user').data().active_user;
+  var $connection = $('#active-user').data('active_user');
   $.ajax({
     method: "POST",
     url: "/users/" + $connection + "/tags",
