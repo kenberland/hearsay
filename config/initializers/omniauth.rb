01ee1802e8c3a9335b47d3a9ceb9f859010b49ba
Kenneth Berland <ken@grandrounds.com>
2016-06-02 06:59:59 -0700

    remove LI; add user_friends

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :developer unless Rails.env.production?
-  provider :linkedin, ENV['LI_CLIENT_ID'], ENV['LI_CLIENT_SECRET'], :scope => 'r_basicprofile r_emailaddress' # r_network
-  provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET'], :scope => 'user_relationships,user_location'
+  provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET'], :scope => 'user_relationships,user_location,user_friends'
 end
