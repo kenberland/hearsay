1937bb0839986d4871dd52a129f1bc78930201e6
Kenneth Berland <ken@grandrounds.com>
2016-06-23 03:36:37 -0700

    add back platforms

---
+++ cordova/platforms/android/assets/www/js/index.js
@@ -0,0 +1,57 @@
+var app = {
+    // Application Constructor
+    initialize: function() {
+        this.bindEvents();
+    },
+    // Bind Event Listeners
+    //
+    // Bind any events that are required on startup. Common events are:
+    // 'load', 'deviceready', 'offline', and 'online'.
+    bindEvents: function() {
+        document.addEventListener('deviceready', this.onDeviceReady, false);
+    },
+    // deviceready Event Handler
+    //
+    // The scope of 'this' is the event. In order to call the 'receivedEvent'
+    // function, we must explicitly call 'app.receivedEvent(...);'
+    onDeviceReady: function() {
+        app.receivedEvent('deviceready');
+    },
+    // Update DOM on a Received Event
+    receivedEvent: function(id) {
+        var parentElement = document.getElementById(id);
+        var listeningElement = parentElement.querySelector('.listening');
+        var receivedElement = parentElement.querySelector('.received');
+
+        listeningElement.setAttribute('style', 'display:none;');
+        receivedElement.setAttribute('style', 'display:block;');
+
+        console.log('Received Event: ' + id);
+    }
+};
+
+app.initialize();
+
+function onSuccess(contacts) {
+    alert('Found ' + contacts.length + ' contacts.');
+};
+
+function onError(contactError) {
+    alert('onError!');
+};
+
+document.addEventListener("deviceready", onDeviceReady, false);
+function onDeviceReady() {
+  console.log(navigator.contacts);
+  doContacts();
+}
+
+function doContacts() {
+  var options      = new ContactFindOptions();
+  options.filter   = "Bob";
+  options.multiple = true;
+  options.desiredFields = [navigator.contacts.fieldType.id];
+  options.hasPhoneNumber = true;
+  var fields       = [navigator.contacts.fieldType.displayName, navigator.contacts.fieldType.name];
+  navigator.contacts.find(fields, onSuccess, onError, options);
+}
