c9dd2ad26dc72ac2e0673e5f1fdd1f71bf31b082
Kenneth Berland <ken@grandrounds.com>
2016-06-26 19:44:17 -0700

    more mocks

--- cordova/www/js/index.js
+++ cordova/www/js/index.js
@@ -18,36 +18,74 @@ var app = {
   }
 };
 
-function onSuccess(contacts) {
-  console.log("fuck" + contacts);
-  var jsonContacts = JSON.stringify(contacts, null, 2);
-  var x = document.getElementsByClassName("contacts");
-  x[0].innerHTML = jsonContacts;
-};
-
-function onError(contactError) {
+var HearsayContacts = {
+  // PROPS
+  contact_ids: null,
+  current_contact: 0,
+  current_contact_detail: {},
+  // METHODS
+  onSuccess: function(contacts) {
+    HearsayContacts.contact_ids = contacts;
+    HearsayContacts.getOne(HearsayContacts.current_contact);
+  },
+  onError: function(contactError) {
     alert('onError!');
+  },
+  onGetOneSuccess: function(contact){
+    HearsayContacts.current_contact_detail = contact[0];
+    HearsayContacts.paintCurrent();
+  },
+  onGetOneError: function(contactError){
+    alert('onError!');
+  },
+  perform: function() {
+    var options      = new ContactFindOptions();
+    options.multiple = true;
+    options.desiredFields = [navigator.contacts.fieldType.id ];
+    var fields = [navigator.contacts.fieldType.id];
+    navigator.contacts.find(fields, this.onSuccess, this.onError, options);
+  },
+  getOne: function(id) {
+    var options = new ContactFindOptions();
+    options.filter = id;
+    var fields = [navigator.contacts.fieldType.id];
+    navigator.contacts.find(fields, this.onGetOneSuccess, this.onGetOneError, options);
+  },
+  paintCurrent: function() {
+    this.paint(this.current_contact_detail);
+  },
+  paint: function(contact) {
+    $('#contact_name').html(contact.displayName);
+  },
+  next: function(){
+    this.current_contact++;
+    this.current_contact = (this.current_contact > (this.contact_ids.length - 1)) ? 0 : this.current_contact;
+    this.getOne(this.current_contact);
+  },
+  previous: function(){
+    this.current_contact--;
+    this.current_contact = (this.current_contact < 0) ? (this.contact_ids.length - 1): this.current_contact;
+    this.getOne(this.current_contact);
+  }
 };
-
-function doContacts() {
-  var options      = new ContactFindOptions();
-  // options.filter  = "";
-  options.multiple = true;
-  options.desiredFields = [navigator.contacts.fieldType.id ];
-  //, navigator.contacts.fieldType.displayName];
-  var fields = [navigator.contacts.fieldType.id];
-  // var fields = [];
-  //, navigator.contacts.fieldType.displayName, navigator.contacts.fieldType.name, navigator.contacts.fieldType.phoneNumbers, navigator.contacts.fieldType.photos];
-  navigator.contacts.find(fields, onSuccess, onError, options);
-}
 
 function onDeviceReady() {
   console.log("device uuid: " + device.uuid);
-  doContacts();
+  HearsayContacts.perform();
 }
+
 
 document.addEventListener("deviceready", onDeviceReady, false);
 app.initialize();
 
 var event = new Event('deviceready');
 document.dispatchEvent(event);
+
+$("#next").click( function() {
+  HearsayContacts.next();
+}
+		);
+$("#previous").click( function() {
+  HearsayContacts.previous();
+}
+		);
