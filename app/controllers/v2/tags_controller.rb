47d7c7225875c9c805b0bce59f711cd04df39da7
Justin <justin@soonahn.ca>
2016-07-12 17:52:41 -0500

    returning the hash array

--- app/controllers/v2/tags_controller.rb
+++ app/controllers/v2/tags_controller.rb
@@ -1,6 +1,11 @@
 class V2::TagsController < ApplicationController
 
   def show
-    render json: {lol: 'fuck you'}
+    tags = Tag.joins(:tag_category).pluck(:tag, '"tag_categories"."category"')
+    tag_hash = tags.each_with_object({}) do |tag, return_hash|
+      return_hash[tag.last.capitalize].nil? ? return_hash[tag.last.capitalize] = [tag.first] : return_hash[tag.last.capitalize].push(tag.first)
+    end
+
+    render json: tag_hash
   end
 end
