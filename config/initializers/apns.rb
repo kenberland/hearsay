e56f42753de9ab3bd2398e19e22a96478c1ae35f
Kenneth Berland <ken@grandrounds.com>
2016-10-24 21:16:49 -0700

    configure apns

--- config/initializers/apns.rb
+++ config/initializers/apns.rb
@@ -1,3 +1,5 @@
-APNS.host = 'gateway.push.apple.com' # gateway.sandbox.push.apple.com is default
-APNS.pem  = '/path/to/pem/file'
+APNS.host = ENV['APNS_HOST'] || 'gateway.sandbox.push.apple.com'
+APNS.pem  = ENV['APNS_PEM_FILE'] || 'foo.pem'
+
+# 'gateway.push.apple.com'
 
