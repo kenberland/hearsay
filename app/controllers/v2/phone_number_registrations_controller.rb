9ecc463c89ccb18b89b25a3297734cc3bed85831
Kenneth Berland <ken@hero.com>
2016-11-07 15:34:19 -0800

    validate on the server side

--- app/controllers/v2/phone_number_registrations_controller.rb
+++ app/controllers/v2/phone_number_registrations_controller.rb
@@ -2,11 +2,20 @@ class V2::PhoneNumberRegistrationsController < ApplicationController
   skip_before_action :verify_authenticity_token
 
   def create
-    result = PhoneNumberRegistration.create phone_number_registration_params
+    params[:device_phone_number] = normalize_us_phone_number(params[:device_phone_number])
+    result = PhoneNumberRegistration.create! phone_number_registration_params
     render json: result
+  rescue ActiveRecord::RecordInvalid => e
+    render json: { error: { RecordInvalid: e.record.errors } }, status: 400
   end
 
   private
+
+  def normalize_us_phone_number(number)
+    Phony.normalize(number, cc: '1')
+  rescue Phony::NormalizationError => e
+    nil
+  end
 
   def phone_number_registration_params
     params.permit(:device_uuid, :device_phone_number)
--- app/models/phone_number_registration.rb
+++ app/models/phone_number_registration.rb
@@ -1,2 +1,9 @@
 class PhoneNumberRegistration < ActiveRecord::Base
+    validate :device_phone_number_plausible, on: :create
+
+    def device_phone_number_plausible
+      unless Phony.plausible?(self.device_phone_number)
+        errors.add(:device_phone_number, 'is not a valid phone number.')
+      end
+    end
 end
