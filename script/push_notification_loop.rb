41e20318ec78ce863530eba40bd4f7f66ec87e83
Kenneth Berland <ken@hero.com>
2016-11-15 21:33:09 -0800

    only push when everything is not null

--- script/push_notification_loop.rb
+++ script/push_notification_loop.rb
@@ -32,15 +32,15 @@ class PushNotificationLoop
         registration_id = Registration.order('created_at desc').find_by_device_uuid(device_uuid).try(:registration_id)
         if registration_id
           puts "notify id: #{registration_id} for user at phone #{phone_number}"
+          push_client.push(registration_id)
+          user_tag.notification_state = :notified
+          user_tag.save!
         else
           puts "no push registration_id for device uuid: #{device_uuid}"
         end
       else
         puts "no device registered for phone #{phone_number}"
       end
-      push_client.push(registration_id)
-      user_tag.notification_state = :notified
-      user_tag.save!
       puts "finished attempting to notify about UserTag #{user_tag.id} #{user_tag.notification_state}"
     else
       puts "no new UserTags"
