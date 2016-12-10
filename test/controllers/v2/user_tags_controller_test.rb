06bfa0b448453c4de9014eb75b7085cd2e2255d6
Kenneth Berland <ken@hero.com>
2016-12-09 20:40:24 -0800

    more tests!

--- test/controllers/v2/user_tags_controller_test.rb
+++ test/controllers/v2/user_tags_controller_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-def my_phone_number
+def target_phone_number
   Faker::PhoneNumber.phone_number.gsub(/\D/,'')
 end
 
@@ -14,9 +14,10 @@ class V2::UserTagsControllerTest < ActionDispatch::IntegrationTest
     params = { 'currentUser' => reg.device_uuid, tag: { id: random_tag } }
     assert_difference('UserTag.count', 0) do
       hearsay_xml_http_request(:post,
-                               user_tags_url(my_phone_number),
+                               user_tags_url(target_phone_number),
                                parameters = params
                               )
+      assert_equal('You cannot tag until you verify your device!', response.body)
       assert_response 400
     end
   end
@@ -25,9 +26,10 @@ class V2::UserTagsControllerTest < ActionDispatch::IntegrationTest
     params = { 'currentUser' => reg.device_uuid, tag: { id: random_tag } }
     assert_difference('UserTag.count', 0) do
       hearsay_xml_http_request(:post,
-                               user_tags_url(my_phone_number),
+                               user_tags_url(target_phone_number),
                                parameters = params
                               )
+      assert_equal('You cannot tag until you verify your device!', response.body)
       assert_response 400
     end
   end
@@ -36,7 +38,7 @@ class V2::UserTagsControllerTest < ActionDispatch::IntegrationTest
       reg = FactoryGirl.create(:phone_number_registration, :verified)
       params = { 'currentUser' => reg.device_uuid, tag: { id: random_tag } }
       hearsay_xml_http_request(:post,
-                       user_tags_url(my_phone_number),
+                       user_tags_url(target_phone_number),
                        parameters = params
                       )
       assert_response 200
@@ -55,8 +57,40 @@ class V2::UserTagsControllerTest < ActionDispatch::IntegrationTest
     end
   end
   test 'allow deleting tags the user puts on another user' do
+    my_tag = random_tag
+    target_user = target_phone_number
+    reg = FactoryGirl.create(:phone_number_registration, :verified)
+    params = { 'currentUser' => reg.device_uuid, tag: { id: my_tag } }
+    hearsay_xml_http_request(:post,
+                             user_tags_url(target_user),
+                             parameters = params
+                            )
+    params = { 'currentUser' => reg.device_uuid }
+    assert_difference('UserTag.count', -1) do
+      hearsay_xml_http_request(:delete,
+                               user_tag_url(target_user, my_tag),
+                               parameters = params
+                              )
+    end
   end
-  test 'restore the soft-deleted tag when it is restored' do
+  test 'deleting a tag actually soft deletes it' do
+    my_tag = random_tag
+    target_user = target_phone_number
+    reg = FactoryGirl.create(:phone_number_registration, :verified)
+    params = { 'currentUser' => reg.device_uuid, tag: { id: my_tag } }
+    hearsay_xml_http_request(:post,
+                             user_tags_url(target_user),
+                             parameters = params
+                            )
+    params = { 'currentUser' => reg.device_uuid }
+    assert_difference('UserTag.unscoped.count', 0) do
+      hearsay_xml_http_request(:delete,
+                               user_tag_url(target_user, my_tag),
+                               parameters = params
+                              )
+    end
+  end
+  test 'restore the soft-deleted tag when it is re-added' do
   end
   test 'disallow deleting tags the user did not put on another user' do
   end
