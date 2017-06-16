17d6c3f2b969b3e760cf9878caf2398bae94fba4
Kenneth Berland <ken@hero.com>
2017-06-15 21:01:59 -0700

    s/.com/.net/g

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock '3.4.0'
 
 set :application, 'hearsay'
-set :repo_url, 'git@hero.com:hearsay.git'
+set :repo_url, 'git@hero.net:hearsay.git'
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
