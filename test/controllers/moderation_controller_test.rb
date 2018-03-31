8c220727125626e8ab9e2912754ed179ec7d848d
Kenneth Berland <berlandk@amazon.com>
2018-03-31 08:51:40 -0700

    add audited to Tag
    add a moderation test

--- test/controllers/moderation_controller_test.rb
+++ test/controllers/moderation_controller_test.rb
@@ -7,4 +7,26 @@ class ModerationControllerControllerTest < ActionDispatch::IntegrationTest
     assert_response 200
     assert_equal(css_select('button.approve').count, 45)
   end
+
+  test 'moderate an approved tag' do
+    moderate('approve')
+  end
+
+  test 'moderate a rejected tag' do
+    moderate('reject')
+  end
+
+
+  def moderate action
+    id = (rand*Tag.count).to_int + 1
+    assert_equal(Tag::UNMODERATED, Tag.find(id).moderation_state)
+    post('/moderate', { tag: { id: id },
+                        moderation_action: action},
+         {'HTTP_REFERER' => 'http://foo.com'}
+        )
+
+    assert_response 302
+    assert_equal(Tag.convert_moderation_intent(action),
+                 Tag.find(id).moderation_state)
+  end
 end
