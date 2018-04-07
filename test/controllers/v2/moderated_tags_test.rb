b2bd6c39a88beab11fc266d0052d3ed6aad01d5d
Kenneth Berland <berlandk@amazon.com>
2018-04-07 09:35:13 -0700

    add new test file

---
+++ test/controllers/v2/moderated_tags_test.rb
@@ -0,0 +1,21 @@
+require 'test_helper'
+
+class V2::ModeratedTagsTest < ActionDispatch::IntegrationTest
+  test 'exactly one new tag is returned when the tag is newly created' do
+    tag_category = random_tag_category
+    target_user = target_phone_number
+    my_tag = Faker::StarWars.character
+    reg = FactoryGirl.create(:phone_number_registration, :verified)
+    params = { 'currentUser' => reg.device_uuid, tag: {
+                 newTag: my_tag,
+                 category: random_tag_category
+               }
+             }
+    hearsay_xml_http_request(:post,
+                             user_tags_url(target_user),
+                             parameters = params
+                            )
+    assert_equal(Hash, JSON.parse(response.body)["new_tag"].class)
+    assert_equal(Tag.last.id, JSON.parse(response.body)["new_tag"]["id"])
+  end
+end
