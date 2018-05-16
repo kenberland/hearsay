ae373db941af9de59032ebd0d50f2381b5d4cd84
Kenneth Berland <berlandk@amazon.com>
2018-05-16 10:23:05 -0700

    add moderation state to the returned tag list from /tags

--- app/controllers/v2/tags_controller.rb
+++ app/controllers/v2/tags_controller.rb
@@ -2,16 +2,16 @@ class V2::TagsController < ApplicationController
 
   def index
     tags = Tag.joins(:tag_category)
-      .pluck(:id, :tag, 'tag_categories.category', 'tag_categories.id')
+      .pluck(:id, :tag, 'tag_categories.category', 'tag_categories.id', :moderation_state)
       .group_by{|c| c[2]}
-
     tag_array = tags.each_with_object([]) do |(key, value), return_array|
       return_array << {
         category: key,
         category_id: value.first[3],
         tags: value.map{|tag| {
                           id: tag[0],
-                          tag: tag[1]
+                          tag: tag[1],
+                          moderation_state: Tag.moderation_text(tag[4]),
                         }
         }
       }
